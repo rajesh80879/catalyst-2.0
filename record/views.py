8024b83d6448250bbbaed0ef621f318d2907fe8e
rajesh80879 <rajeshgupta80879@gmail.com>
2024-02-07 22:53:52 +0530

    Updated upload-data csv

--- record/views.py
+++ record/views.py
@@ -1,4 +1,7 @@
 from decimal import Decimal
+import math
+import time
+import uuid
 from django.shortcuts import render, redirect
 import csv
 from .models import Record
@@ -9,6 +12,8 @@ from rest_framework.response import Response
 from .models import Record
 from .serializers import RecordSerializer
 from django.db.models import Q
+import pandas as pd
+from django.db import transaction
 
 # Create your views here.
 
@@ -48,96 +53,64 @@ def query_builder(request):
 
 def upload_data(request):
     if request.method == "POST":
-        # Set the chunk size
-        chunk_size = 100000
+        try:
+            file = request.FILES["file"]
+            chunk_size = 100000
+            records_to_create = []
 
-        file = request.FILES.get("file")
+            with transaction.atomic():
+                df = pd.read_csv(file)
 
-        if file:
+                for index, row_values in df.iterrows():
+                    record = Record()
+                    record.name = row_values.get("name")
+                    record.domain = row_values.get("domain")
+                    record.industry = row_values.get("industry")
+                    record.size_range = row_values.get("size range")
+                    record.linkedin_url = row_values.get("linkedin url")
 
-            try:
-                decoded_chunk = file.read().decode("utf-8")
-            except UnicodeDecodeError as e:
-                messages.error(request, "Error decoding file")
-                return redirect("dashboard")
-
-            reader_chunk = csv.reader(io.StringIO(decoded_chunk))
-            headers = next(reader_chunk)
-
-            try:
-                records_to_create = []
-                rows_processed = 0
-
-                for row_values in reader_chunk:
-                    row = dict(zip(headers, row_values))
-
-                    name = row.get("name")
-                    domain = row.get("domain")
-                    year_founded_str = row.get("year founded", "")
-                    industry = row.get("industry")
-                    size_range = row.get("size range")
-                    linkedin_url = row.get("linkedin url")
-                    current_employees = (
-                        int(row.get("current employee estimate"))
-                        if row.get("current employee estimate")
+                    record.year_founded = (
+                        int(row_values.get("year founded"))
+                        if not pd.isna(row_values.get("year founded"))
                         else None
                     )
-                    total_employees = (
-                        int(row.get("total employee estimate"))
-                        if row.get("total employee estimate")
+                    record.current_employees = (
+                        int(row_values.get("current employee estimate"))
+                        if row_values.get("current employee estimate")
+                        and not pd.isna(row_values.get("current employee estimate"))
+                        else None
+                    )
+                    record.total_employees = (
+                        int(row_values.get("total employee estimate"))
+                        if row_values.get("total employee estimate")
+                        and not pd.isna(row_values.get("total employee estimate"))
                         else None
                     )
 
-                    if "locality" in row and row["locality"]:
-                        locality_parts = row["locality"].split(",")
-                        city = locality_parts[0].strip()
-                        state = locality_parts[1].strip()
-                        country = locality_parts[2].strip()
+                    locality = row_values.get("locality")
+                    if isinstance(locality, str):
+                        locality_parts = locality.split(",")
+                        record.city = locality_parts[0].strip()
+                        record.state = locality_parts[1].strip()
+                        record.country = locality_parts[2].strip()
                     else:
-                        city = ""
-                        state = ""
-                        country = ""
-
-                    year_founded = int(year_founded_str) if year_founded_str else None
-
-                    record = Record(
-                        name=name,
-                        domain=domain,
-                        year_founded=year_founded,
-                        industry=industry,
-                        size_range=size_range,
-                        city=city,
-                        state=state,
-                        country=country,
-                        linkedin_url=linkedin_url,
-                        current_employees=current_employees,
-                        total_employees=total_employees,
-                    )
+                        record.city = ""
+                        record.state = ""
+                        record.country = ""
 
                     records_to_create.append(record)
-                    rows_processed += 1
-
-                    print(f"Total rows processed: {rows_processed}")
-
-                    if rows_processed % chunk_size == 0:
+                    if len(records_to_create) >= chunk_size:
                         Record.objects.bulk_create(records_to_create)
                         records_to_create = []
 
                 if records_to_create:
                     Record.objects.bulk_create(records_to_create)
-                    messages.success(request, "Data uploaded successfully")
-                    return redirect("dashboard")
-                else:
-                    messages.error(request, "No records found in the file")
 
-            except Exception as e:
-                messages.error(
-                    request, "Something went wrong while processing the file"
-                )
+                messages.success(request, "Data uploaded successfully")
                 return redirect("dashboard")
 
-        else:
-            messages.error(request, "Please upload a valid document")
+        except Exception as e:
+            messages.error(request, "Something went wrong while processing the file")
             return redirect("dashboard")
 
 
